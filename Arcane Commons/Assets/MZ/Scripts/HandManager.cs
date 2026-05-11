6447ef4c1b68df0b97a5f8c860a903a3f96d1b0e
DESKTOP-41DHHHO\GA3 <h1202415026@hiratagakuen.onmicrosoft.com>
2026-05-11 14:51:04 +0900

    手札にカードを追加(目視)・（カードをボタン化）

--- Arcane Commons/Assets/MZ/Scripts/HandManager.cs
+++ Arcane Commons/Assets/MZ/Scripts/HandManager.cs
@@ -1,14 +1,25 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HandManager : MonoBehaviour
 {
     public List<CardData> hand = new List<CardData>();
 
+    public GameObject cardPrefab;
+
+    public Transform handArea;
+
     public void AddCard(CardData card)
     {
         hand.Add(card);
 
         Debug.Log(card.cardName + " ‚ðŽèŽD‚É’Ç‰Á");
+
+        GameObject cardObject = Instantiate(cardPrefab, handArea);
+
+        TMP_Text text = cardObject.GetComponentInChildren<TMP_Text>();
+
+        text.text = card.cardName;
     }
 }
